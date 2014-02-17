019d5ad94fd6951f848faf11ff74ce6d367db04b
Stuart Sandine <stuajc@gmail.com>
2014-02-17 13:56:47 -0500

    Chapter 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,45 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(RESET)
+  @8192   //first 4 lns just setting @counter to data value
+  D=A
+  @counter
+  M=D
+  @16384  //set @screen's value to the memory address where screen map begins
+  D=A
+  @screen
+  M=D
+  @INFINITELOOPLOL
+  0;JMP
+(INFINITELOOPLOL)
+  @counter
+  D=M
+  @RESET 
+  D;JLE    //if counter is <= 0, reset it
+  @KBD     //grab keyboard state
+  D=M      //stash in d
+  @COLORBLACK
+  D;JNE    //jump to colorblack if keyboard state != 0
+  @COLORWHITE
+  0;JMP    //else jump to colorwhite
+(COLORBLACK)
+  @screen
+  A=M
+  M=-1
+  @counter
+  M=M-1
+  @screen
+  M=M+1
+  @INFINITELOOPLOL
+  0;JMP
+(COLORWHITE)
+  @screen
+  A=M
+  M=0
+  @counter
+  M=M-1
+  @screen
+  M=M+1
+  @INFINITELOOPLOL
+  0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -5,5 +5,22 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
-
-// Put your code here.
+  
+  @R2    //grab memory address for result
+  M=0    //initialize result to 0
+(LOOP)
+  @R1    //grab second num
+  D=M    //set d to second num
+  @END
+  D;JLE  //if second num <=0, jump to end
+  @R0    //grab first num
+  D=M    //set d to first num
+  @R2    //grab result
+  M=M+D  //result += first num
+  @R1    //grab second num
+  M=M-1  //second num--
+  @LOOP
+  0;JMP  //loop again
+(END)
+  @END
+  0;JMP
